67f291971843e3dd26849b142fa78956443ff06c
Arthur Oliveira <arthurolivsira@gmail.com>
2023-06-23 22:05:01 -0300

    Adicionando funcionalidade para modificar quantidade de um item

--- main.js
+++ main.js
@@ -1,40 +1,54 @@
 const form = document.getElementById('novoItem')
-const lista = document.getElementById("lista")
-const itens = JSON.parse(localStorage.getItem("itens")) || []
+const lista = document.getElementById('lista')
+const itens = JSON.parse(localStorage.getItem('itens')) || []
 
 itens.forEach(elemento => {
   criaElemento(elemento)
-});
+})
 
-form.addEventListener("submit", (evento)=> {
+form.addEventListener('submit', evento => {
   evento.preventDefault()
 
-  const nome = evento.target.elements["nome"]
-  const quantidade = evento.target.elements["quantidade"]
-
+  const nome = evento.target.elements['nome']
+  const quantidade = evento.target.elements['quantidade']
+  const existe = itens.find(elemento => elemento.nome === nome.value)
   const itemAtual = {
-    "nome": nome.value,
-    "quantidade": quantidade.value
+    nome: nome.value,
+    quantidade: quantidade.value
   }
-  criaElemento(itemAtual)
 
-  itens.push(itemAtual)
+  if (existe) {
+    itemAtual.id = existe.id
 
-  localStorage.setItem("itens", JSON.stringify(itens))
+    atualizaElemento(itemAtual)
+  } else {
+    itemAtual.id = itens.length
 
-  nome.value = ""
-  quantidade.value = ""
+    criaElemento(itemAtual)
+    itens.push(itemAtual)
+  }
+
+  localStorage.setItem('itens', JSON.stringify(itens))
+
+  nome.value = ''
+  quantidade.value = ''
 })
 
 function criaElemento(item) {
   const novoItem = document.createElement('li')
-  novoItem.classList.add("item")
+  novoItem.classList.add('item')
 
-  const numeroItem = document.createElement("strong")
+  const numeroItem = document.createElement('strong')
   numeroItem.innerHTML = item.quantidade
 
-  novoItem.appendChild(numeroItem) 
+  numeroItem.dataset.id = item.id
+
+  novoItem.appendChild(numeroItem)
   novoItem.innerHTML += item.nome
 
   lista.appendChild(novoItem)
+}
+
+function atualizaElemento(item) {
+  document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
 }
