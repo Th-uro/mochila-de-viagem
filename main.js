35a6dbacd12e3d87929e1a372c4e718bdf365e60
Arthur Oliveira <arthurolivsira@gmail.com>
2023-06-23 22:32:38 -0300

    Adicionando um item a mochila

--- main.js
+++ main.js
@@ -21,6 +21,7 @@ form.addEventListener('submit', evento => {
     itemAtual.id = existe.id
 
     atualizaElemento(itemAtual)
+    itens[existe.id] = itemAtual
   } else {
     itemAtual.id = itens.length
 
@@ -50,5 +51,6 @@ function criaElemento(item) {
 }
 
 function atualizaElemento(item) {
-  document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade
+  document.querySelector("[data-id='" + item.id + "']").innerHTML =
+    item.quantidade
 }
